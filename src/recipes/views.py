b7d4248f5f2c1acec0a03f21287a4cb2c916e73b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-14 21:21:31 -0700

    Updated route in views.py

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/recipes_home.html')
