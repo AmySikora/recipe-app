ee44d4a34194b1215c40fc028ce7c193268787ab
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 20:18:43 -0700

    added images to recipes list

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home # type: ignore
+from .views import home, RecipeListView # type: ignore
+
+app_name = "recipes"
 
 urlpatterns = [
     path('', home, name='home'),
+    path("list/", RecipeListView.as_view(), name="list"),
 ]
