7717809520088afa71357644a8292ccd1fee992f
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 21:33:20 -0700

    updated view.py typo

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView   #to display lists
-from .models import Recipe        
+from django.views.generic import ListView, DetailView
+from .models import Recipe  
 
-# Create your views here.
-class RecipeListView(ListView):           #class-based view
-   model = Recipe                         #specify model
-   template_name = 'recipes/recipes_list.html' 
-   
+# List View for Recipes
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/recipes_list.html'
+
+# Detail View for a Single Recipe
 class RecipeDetailView(DetailView):
-    model = Recipetemplate_name = 'recipes/detail.html'
+    model = Recipe  
     template_name = 'recipes/detail.html'
 
+# Home View
 def home(request):
     return render(request, 'recipes/recipes_home.html')
