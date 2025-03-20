d029d36e817091e230db09ef278989ff21654b7d
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-19 17:33:13 -0700

    added recipres list to views.py

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView   #to display lists
+from .models import Recipe        
 # Create your views here.
+class RecipeListView(ListView):           #class-based view
+   model = Recipe                         #specify model
+   template_name = 'recipes/recipes_list.html'    #specify template 
+
 def home(request):
     return render(request, 'recipes/recipes_home.html')
