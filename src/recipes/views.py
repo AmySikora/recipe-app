d8b20ea391b44da5fddf5de70cc787fb5cd4c8c2
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-21 13:49:47 -0700

    updated to protect class-based views

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe  # Ensure Recipe is correctly imported
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # List View for Recipes
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
+#class-based “protected” view    
     model = Recipe
+ #specify model   
     template_name = 'recipes/recipes_list.html'
 
 # Detail View for a Single Recipe
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe  
     template_name = 'recipes/detail.html'
 
