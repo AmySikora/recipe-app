04c01d781af7852d42db2fa43002678425d5ae00
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-26 23:03:34 -0700

    updated urls

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -5,7 +5,7 @@ app_name = "recipes"
 
 urlpatterns = [
     path('', home, name='home'),
-    path('list/', RecipeListView.as_view(), name='recipe_list'),
-    path('list/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipes/', RecipeListView.as_view(), name='recipe_list'),  # changed from 'list/' to 'recipes/'
+    path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),  # changed from 'list/<int:pk>/' to 'recipes/<int:pk>/'
     path('records/', records, name='records'),  
 ]
