ca776625ce2e3ebc0c95a79cb50093996ef20f6c
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-26 21:02:06 -0700

    updated to add sections for all recipes and search

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -37,49 +37,63 @@ def home(request):
 def records(request):
     form = RecipeSearchForm(request.POST or None)
     recipes_df = None
+    all_recipes_df = None
     chart = None
-    qs = Recipe.objects.none()
 
-    if request.method == 'POST' and form.is_valid():
-        search_term = form.cleaned_data.get('search_term')
-        chart_type = form.cleaned_data.get('chart_type')
+    if request.method == 'POST':
+        if 'search' in request.POST and form.is_valid():
+            search_term = form.cleaned_data.get('search_term')
+            chart_type = form.cleaned_data.get('chart_type')
 
-        if search_term:
             qs = Recipe.objects.filter(
                 name__icontains=search_term
             ) | Recipe.objects.filter(
                 ingredients__icontains=search_term
             )
-            
-        else:
+
+            if qs.exists():
+                data = []
+                labels = []
+
+                for recipe in qs:
+                    link = f'<a href="{reverse("recipes:recipe_detail", args=[recipe.id])}">{recipe.name}</a>'
+                    data.append({
+                        'Name': link,
+                        'Ingredients': recipe.ingredients,
+                        'Cooking Time (min)': recipe.cooking_time,
+                        'Difficulty': recipe.difficulty,
+                        'Ingredient Count': len(recipe.ingredients.split(','))
+                    })
+                    labels.append(recipe.name)
+
+                df = pd.DataFrame(data)
+                recipes_df = df.to_html(escape=False)
+                chart = get_chart(chart_type, df, labels=labels)
+
+        elif 'show_all' in request.POST:
             qs = Recipe.objects.all()
 
-        if qs.exists():
-            data = []
-            labels = []
+            if qs.exists():
+                all_data = []
 
-            for recipe in qs:
-                link = f'<a href="{reverse("recipes:recipe_detail", args=[recipe.id])}">{recipe.name}</a>'
-                data.append({
-                    'Name': link,
-                    'Ingredients': recipe.ingredients,
-                    'Cooking Time (min)': recipe.cooking_time,
-                    'Difficulty': recipe.difficulty
-                })
-                labels.append(recipe.name)
+                for recipe in qs:
+                    link = f'<a href="{reverse("recipes:recipe_detail", args=[recipe.id])}">{recipe.name}</a>'
+                    all_data.append({
+                        'Name': link,
+                        'Ingredients': recipe.ingredients,
+                        'Cooking Time (min)': recipe.cooking_time,
+                        'Difficulty': recipe.difficulty,
+                        'Ingredient Count': len(recipe.ingredients.split(','))
+                    })
 
-            recipes_df = pd.DataFrame(data)
-            chart = get_chart(chart_type, recipes_df, labels=labels)
-            recipes_df = recipes_df.to_html(escape=False)
+                all_recipes_df = pd.DataFrame(all_data).to_html(escape=False)
 
-    context = {
+    return render(request, 'recipes/records.html', {
         'form': form,
         'recipes_df': recipes_df,
         'chart': chart,
-    }
-
-    return render(request, 'recipes/records.html', context)
-
+        'all_recipes_df': all_recipes_df
+    })
 
 # Login View
 def login_view(request):
