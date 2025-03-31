6b2ede4fbd4e47d0f0c08f0cff898d7166f83bbf
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-30 21:28:14 -0700

    Use dj_database_url for Heroku PostgreSQL

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from .env
 load_dotenv()
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', 
+        conn_max_age=600
+    )
 }
 
 
